f14f0d0198de5c3ebdbbb4b0227ec3d7a8e1306b
IGour <ijgour@gmail.com>
2019-03-29 10:31:23 +0530

    java config "constructor injection"

--- src/main/java/gour/indrajeet/service/CustomerServiceImpl.java
+++ src/main/java/gour/indrajeet/service/CustomerServiceImpl.java
@@ -2,15 +2,18 @@ package gour.indrajeet.service;
 
 import gour.indrajeet.model.Customer;
 import gour.indrajeet.repository.CustomerRepository;
-import gour.indrajeet.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
     // here we will have all the service logic on service tier
 
-    //    custoomer repo implementation
+    // customer repo implementation
     private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
