de23fa46235dadd62d701cf50d39dc4ce2d89993
IGour <ijgour@gmail.com>
2019-03-29 18:30:26 +0530

    Bean scope like define the singleton

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,6 +8,10 @@ public class Application {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         CustomerService customerService = applicationContext.getBean("customerService",CustomerService.class);
+        CustomerService customerService1 = applicationContext.getBean("customerService",CustomerService.class);
+//        Just to check the scope of the bean default if singleton, means will get the same object return both the time
+        System.out.println(customerService);
+        System.out.println(customerService1);
 //        CustomerService customerService = new CustomerServiceImpl();
 
         System.out.println(customerService.findAll().get(0).getFirstname());
--- src/main/java/gour/indrajeet/service/CustomerServiceImpl.java
+++ src/main/java/gour/indrajeet/service/CustomerServiceImpl.java
@@ -3,11 +3,16 @@ package gour.indrajeet.service;
 import gour.indrajeet.model.Customer;
 import gour.indrajeet.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
+//@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) // or use the configurableBeanFactory class instead of defining
+// the constact directly like singleton
 public class CustomerServiceImpl implements CustomerService {
     // here we will have all the service logic on service tier
 
